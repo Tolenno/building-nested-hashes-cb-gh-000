9ebdc1389e54c775703567efb5465f55c282a839
Matt Hough <matt@hough.tv>
2017-09-29 22:31:28 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -4,16 +4,16 @@ def first_challenge
       patriarch: {
         name: "Lord Montague",
         age: "53"
-      }
+      },
       matriarch: {
         name: "Lady Montague",
         age: "54"
-      }
+      },
       hero: {
         name: "Romeo",
         age: "15",
         status: "alive"
-      }
+      },
       hero_friends: [
         {
           name: "Benvolio",
@@ -31,22 +31,22 @@ def first_challenge
         patriarch: {
           name: "Lord Capulet",
           age: "50"
-        }
+        },
         matriarch: {
           name: "Lady Capulet",
           age: "52"
-        }
+        },
         heroine: {
           name: "Juliet",
           age: "15",
           status: "alive"
-        }
+        },
         hero_friends: [
           {
             name: "Mercutio",
             age: "18",
             attitude: "hot-headed"
-          }
+          },
           {
             name: "Nurse",
             age: "44",
